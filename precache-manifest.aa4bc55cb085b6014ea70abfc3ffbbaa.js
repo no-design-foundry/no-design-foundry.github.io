0272514039d0524e1dcb2fc3668a906bf5bf2a27
jansindl3r <jansindl3r@gmail.com>
2021-12-23 00:07:30 +0100

    Updates

---
+++ precache-manifest.aa4bc55cb085b6014ea70abfc3ffbbaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527526fb0ab317ef85d9422569972188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00641e2cad259485dad5",
+    "url": "/static/js/2.f1a8f39f.chunk.js"
+  },
+  {
+    "revision": "ab867c9bb4cf4086805d2d50c359c72c",
+    "url": "/static/js/2.f1a8f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f295c42f1b50b46b0d97",
+    "url": "/static/js/main.ceb68938.chunk.js"
+  },
+  {
+    "revision": "7a88b8355c37a255338b",
+    "url": "/static/js/runtime-main.0702a5c9.js"
+  }
+]);

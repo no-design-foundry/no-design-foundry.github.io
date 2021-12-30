9d5d51621031a52d70e7dd7b1e61e5eab4fe5143
jansindl3r <jansindl3r@gmail.com>
2021-12-30 16:11:22 +0100

    build folder

---
+++ build/precache-manifest.d8c5500d558ac80c47eb0532cfa1c488.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3185cab9a232ed2d13663636c1a2a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e714e94233d3f8c1ecc",
+    "url": "/static/js/2.d2c9229a.chunk.js"
+  },
+  {
+    "revision": "ab867c9bb4cf4086805d2d50c359c72c",
+    "url": "/static/js/2.d2c9229a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41945f1efbcb89581fda",
+    "url": "/static/js/main.fe17c5b0.chunk.js"
+  },
+  {
+    "revision": "7a88b8355c37a255338b",
+    "url": "/static/js/runtime-main.0702a5c9.js"
+  }
+]);

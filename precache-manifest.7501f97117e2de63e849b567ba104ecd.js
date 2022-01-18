fe8dc487d75b4212dae44a4cbaf64a8a4be7777f
jansindl3r <jansindl3r@gmail.com>
2022-01-18 20:55:18 +0100

    Updates

--- precache-manifest.7501f97117e2de63e849b567ba104ecd.js
+++ precache-manifest.7501f97117e2de63e849b567ba104ecd.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e09a57dbffe85c814380f78c0f75ba0a",
+    "revision": "a3b6cff83863355ef085ddc47cd70af3",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2a12cf2b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aeaa2a282a82a0297d48",
-    "url": "/static/js/main.19fc7984.chunk.js"
+    "revision": "0a2eae26c80abd8d0695",
+    "url": "/static/js/main.8cd3e462.chunk.js"
   },
   {
     "revision": "7a88b8355c37a255338b",
